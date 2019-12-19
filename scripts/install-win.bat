e44d95dee55f3fda70948143a6ed492179e5529c
Sean Gibbons <seangibbon42@gmail.com>
2019-12-18 22:19:45 -0500

    Update windows install script

--- scripts/install-win.bat
+++ scripts/install-win.bat
@@ -7,7 +7,7 @@ mkdir output
 python -m venv venv
 CALL venv\Scripts\activate.bat
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 :END
